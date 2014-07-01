e3ffcd827ded4943a8273e302ca261f28d55b93a
Juraj Petrik <juraj.petrik@taqtile.com>
2014-06-30 23:34:11 -0300

    got jade rendering to work

--- cvprovider-memory.js
+++ cvprovider-memory.js
@@ -1,21 +1,37 @@
 CVProvider = function(){};
 CVProvider.prototype.dummyData = {
 profilePicture: 'images/cthulu.png',
-name: 'C\'thulu',
+name: 'Juraj Petrik',
 cell: '+55 11 905970953',
-email: 'Cthulu@gmail.com',
-objective: 'world domination',
-education:[
+email: 'jurajpetrik77@gmail.com',
+github: 'https://github.com/jurajpetrik',
+objective: 'colonization of the solar system',
+blocks:
+[{
+	headline: 'education',
+	children:[
 	{
-		name: 'Harvard',
-		description: 'Aerospace engineering'
+		title: 'Via University College',
+		time: '2011-2014(ongoing)',
+		content: 'ICT Engineering'
+	}]
+},
+{
+	headline: 'work experience',
+	children:[
+	{
+		title: 'Ifactory Solutions',
+		time: '2013-2014',
+		content: 'not much'
 	},
 	{
-		name: 'Via University College',
-		description: 'Majored in smoking weed and not giving a fuck'
-	}
-]
+		title: 'Taqtile',
+		time: 'Feb 2014 - August 2014',
+		content: 'Majored in doing fuck all'
+	}]
 }
+]
+};
 
 CVProvider.prototype.findById = function(id, callback) {
   var result = this.dummyData;
