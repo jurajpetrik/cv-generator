d1af69df3824ae392c292c8295238cf5befe49f0
Juraj Petrik <jurajpetrik77@gmail.com>
2014-08-15 00:13:11 +0200

    added github icon

--- cvprovider-memory.js
+++ cvprovider-memory.js
@@ -5,42 +5,58 @@ name: 'Juraj Petrík',
 cell: '+55 11 905970953',
 email: 'jurajpetrik77@gmail.com',
 github: 'https://github.com/jurajpetrik',
-objective: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+linkedin: 'https://www.linkedin.com/pub/juraj-petrik/67/8a8/b59',
+objective: 'stuff',
 blocks:
 [{
-	headline: 'Education',
-	children:[
-	{
-		title: 'Via University College',
-		time: '2011-2014(ongoing)',
-		content: 'ICT Engineering'
-	}]
-},
-{
 	headline: 'Work experience',
 	children:[
 	{
 		title: 'Taqtile',
-		titleOverlay: 'Software developer',
-		time: 'Feb 2014 - August 2014',
-		content: 'iOs development<br  />C# backend'
+		titleOverlay: 'Sao Paulo, Brazil',
+		secondaryTitle: 'Feb 2014 - August 2014',
+		content: [
+			'worked on huge iOs app for - ecommerce market leader in South America',
+			'frontend in Objective C, using XIB files instead of Storyline',
+			'worked on C# backend for said applications that must handle millions of users',			]
 	},
 	{
 		title: 'Ifactory Solutions',
-		time: '2013-2014',
-		content: 'not much'
+		titleOverlay: 'Fortaleza, Brazil',
+		secondaryTitle: '2013-2014',
+		content: ['worked on C# and Java backend',
+				'participated in website UI testing']
 	}	
 	]
 },
 {
-	headline: 'Project',
+	headline: 'Projects',
 	children:[
 	{
-		title: 'World peace',
-		time: '2002-2014',
-		content: 'World leader'
+		title: 'CV generator',
+		link: 'https://github.com/jurajpetrik/cv-generator',
+		content: ['Generates CV from your personal info in JSON. Using Node.JS, Express, Jade.',
+		'Used to generate this very CV']
+	},
+	{
+		title: 'Voice call in Browser',
+		link: 'https://github.com/jurajpetrik/callMe',
+		content: ['Voice calling in browser using HTML 5 Audio API, Node.js and Socket.io to',
+		' Semi-successful, works with long delays in sound.']	
 	}
 	]
+},
+{
+	headline: 'Education',
+	children:[
+	{
+		title: 'Via University College - ICT Engineering',
+		titleOverlay: 'Horsens, Denmark',
+		time: '2011-2015(ongoing)',
+		content: [
+			'I am currently still doing my bachelors in Information Communication Technology Engineering. At the moment I am on Erasmus exchange program in Fontys, Netherlands'
+				]
+	}]
 }
 ]
 };
